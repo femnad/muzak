39b71c307a44fadd22c64cf79114f9e1ac3c4016
fcd <git@fcd.dev>
2023-05-23 23:23:04 +0100

    upgrade module versions

--- terraform/muzak.tf
+++ terraform/muzak.tf
@@ -4,7 +4,7 @@ terraform {
 
 module "instance-module" {
   source      = "femnad/instance-module/gcp"
-  version     = "0.12.0"
+  version     = "0.13.0"
   github_user = "femnad"
   project     = "foolproj"
   ssh_user    = var.ssh_user
@@ -13,7 +13,7 @@ module "instance-module" {
 
 module "dns-module" {
   source           = "femnad/dns-module/gcp"
-  version          = "0.4.0"
+  version          = "0.5.0"
   dns_name         = var.dns_name
   instance_ip_addr = module.instance-module.instance_ip_addr
   managed_zone     = var.managed_zone
@@ -21,15 +21,13 @@ module "dns-module" {
 }
 
 module "firewall-module" {
-  version = "0.5.0"
+  version = "0.6.0"
   source  = "femnad/firewall-module/gcp"
   project = var.project
   network = module.instance-module.network_name
   self_reachable = {
     "443" = "tcp"
     "22" = "tcp"
-  }
-  world_reachable = {
   }
   ip_mask = var.managed_connection ? 29 : 32
   ip_num  = var.managed_connection ? 7 : 1
